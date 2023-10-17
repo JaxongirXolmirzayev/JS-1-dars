1d9369031bbe70908e4bb5f4a512e54cfd871803
Jaxongir Xolmirzayev <147791264+JaxongirXolmirzayev@users.noreply.github.com>
2023-10-17 22:12:13 +0300

    JS 1-dars
    27-guruh

---
+++ JS 1-dars/js/main.js
@@ -0,0 +1,51 @@
+/*console.log("Men console oynasida ko'rinaman!");*/
+
+/*document.write("men body oynasida ko'rinaman!");*/
+
+/*alert("Saytimizga hush keibsiz");*/
+
+/*
+console.log(5+5*5/5+100);*/
+
+
+// let x = 5;
+// let y = 2;
+// let z =10;
+// x = 7;
+/*let x = 2,y = 5, z = 10;*/
+
+/*document.write(x+y+z);*/
+
+/*let salom = "asalomalikum"
+
+document.write(salom);*/
+
+/*let rang = "#f1f1f1";
+document.write(rang);*/
+
+
+
+/*let x = 5; */              /* number*/
+// let y = "JavaScript";    /*  string*/
+
+/*document.write(typeof y);*/
+
+
+
+/*let x = 5;*/
+
+/*x+=55;*/     /*x=5+55*/
+
+let x = 5;
+/*x-=7;*/
+/*x/=2;*/
+
+/*let qoldiq = x%5;*/
+x--;
+x--;
+x--;
+x--;
+x--; /*block*/
+
+document.write(x);
+
